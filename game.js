bf99973398cf075b8e5d893e6a9020ab5ff51a02
Koustav Samaddar <koustav201197@gmail.com>
2018-12-26 12:12:27 +0400

    Tested keyboard interaction
    1. Tested keyboard interaction in gameLoop
    2. Made `ctx` global to have easier to read functions

--- game.js
+++ game.js
@@ -1,15 +1,19 @@
+const Mousetrap = require('mousetrap')
+
 const W = 800
 const H = 600
-const DEBUG = false
+const DEBUG = true
 
-function drawCircle(ctx, x, y, radius, color='black') {
+let ctx
+
+function drawCircle(x, y, radius, color='black') {
   ctx.beginPath()
   ctx.arc(x, y, radius, 0, Math.PI * 2, false)
   ctx.strokeStyle = color
   ctx.stroke()
 }
 
-function drawLine(ctx, xi, yi, xf, yf, color='black') {
+function drawLine(xi, yi, xf, yf, color='black') {
   ctx.beginPath()
   ctx.moveTo(xi, yi)
   ctx.lineTo(xf, yf)
@@ -17,28 +21,31 @@ function drawLine(ctx, xi, yi, xf, yf, color='black') {
   ctx.stroke()
 }
 
-function drawRectangle(ctx, x, y, w, h, color='black') {
+function drawRectangle(x, y, w, h, color='black') {
   ctx.strokeStyle = color
   ctx.strokeRect(x, y, w, h)
 }
 
-function gameInit(ctx) {
+function gameInit() {
   // Testing basic shape drawing functionality
-  drawCircle(ctx, W / 2, H / 2, 50, 'blue')
-  drawLine(ctx, 0, 0, 800, 600, 'orange')
-  drawRectangle(ctx, 80, 60, 40, 30, 'green')
+  drawCircle(W / 2, H / 2, 50, 'blue')
+  drawLine(0, 0, 800, 600, 'orange')
+  drawRectangle(80, 60, 40, 30, 'green')
 }
 
-function gameLoop(ctx) {
+function gameLoop() {
   // Main game loop
+  Mousetrap.bind('up', () => {
+      drawCircle(100, 75, 10)
+  })
   setTimeout(gameLoop, 1000)
 }
 
 function gameMain() {
-  const ctx = document.getElementById('gameScreen').getContext('2d')
+  ctx = document.getElementById('gameScreen').getContext('2d')
 
-  gameInit(ctx)
-  gameLoop(ctx)
+  gameInit()
+  gameLoop()
 }
 
 // To export variables to main.js
