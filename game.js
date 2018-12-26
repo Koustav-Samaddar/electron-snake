961cbfbfa2f9fc46f86c04387ac2074e57fa712c
Koustav Samaddar <koustav201197@gmail.com>
2018-12-26 12:22:01 +0400

    Added `isfill` option to draw functions
    Also moved around a few lines of code

--- game.js
+++ game.js
@@ -2,15 +2,21 @@ const Mousetrap = require('mousetrap')
 
 const W = 800
 const H = 600
-const DEBUG = true
+const DEBUG = false
 
 let ctx
 
-function drawCircle(x, y, radius, color='black') {
+function drawCircle(x, y, radius, color='black', isfill=false) {
   ctx.beginPath()
   ctx.arc(x, y, radius, 0, Math.PI * 2, false)
-  ctx.strokeStyle = color
-  ctx.stroke()
+  
+  if (isfill) {
+    ctx.fillStyle = color
+    ctx.fill()
+  } else {
+    ctx.strokeStyle = color
+    ctx.stroke()
+  }
 }
 
 function drawLine(xi, yi, xf, yf, color='black') {
@@ -21,16 +27,24 @@ function drawLine(xi, yi, xf, yf, color='black') {
   ctx.stroke()
 }
 
-function drawRectangle(x, y, w, h, color='black') {
-  ctx.strokeStyle = color
-  ctx.strokeRect(x, y, w, h)
+function drawRectangle(x, y, w, h, color='black', isfill=false) {
+  if (isfill) {
+    ctx.fillStyle = color
+    ctx.fillRect(x, y, w, h)
+  } else {
+    ctx.strokeStyle = color
+    ctx.strokeRect(x, y, w, h)
+  }
 }
 
 function gameInit() {
+  // Getting context from canvas element
+  ctx = document.getElementById('gameScreen').getContext('2d')
+
   // Testing basic shape drawing functionality
-  drawCircle(W / 2, H / 2, 50, 'blue')
-  drawLine(0, 0, 800, 600, 'orange')
-  drawRectangle(80, 60, 40, 30, 'green')
+  // drawCircle(W / 2, H / 2, 50, 'blue')
+  // drawLine(0, 0, 800, 600, 'orange')
+  // drawRectangle(80, 60, 40, 30, 'green')
 }
 
 function gameLoop() {
@@ -38,12 +52,13 @@ function gameLoop() {
   Mousetrap.bind('up', () => {
       drawCircle(100, 75, 10)
   })
+
+  // Clear screen
+  drawRectangle(0, 0, 800, 600, 'black', true)
   setTimeout(gameLoop, 1000)
 }
 
 function gameMain() {
-  ctx = document.getElementById('gameScreen').getContext('2d')
-
   gameInit()
   gameLoop()
 }
