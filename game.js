d0e9a15b951f0f042d9cbaaf2135cc01dd1ad230
Koustav Samaddar <koustav201197@gmail.com>
2018-12-26 17:07:13 +0400

    Created basic interactive program
    Gives more structure to the program

--- game.js
+++ game.js
@@ -1,10 +1,13 @@
 const Mousetrap = require('mousetrap')
 
-const W = 800
-const H = 600
-const DEBUG = false
+const WIDTH = 800
+const HEIGHT = 600
+const SPEED = 1
+const DEBUG = true
 
 let ctx
+const objs = {}
+const keystate = {}
 
 function drawCircle(x, y, radius, color='black', isfill=false) {
   ctx.beginPath()
@@ -40,22 +43,53 @@ function drawRectangle(x, y, w, h, color='black', isfill=false) {
 function gameInit() {
   // Getting context from canvas element
   ctx = document.getElementById('gameScreen').getContext('2d')
+  
+  // Initialising control state flags
+  keystate.ver  = 0
+  keystate.hor  = 0
 
-  // Testing basic shape drawing functionality
-  // drawCircle(W / 2, H / 2, 50, 'blue')
-  // drawLine(0, 0, 800, 600, 'orange')
-  // drawRectangle(80, 60, 40, 30, 'green')
+  // Initialising player object
+  objs.player = {
+    'shape': 'circle',
+    'draw': drawCircle,
+    'drawArgs': [WIDTH/2, HEIGHT/2, 5, 'blue', true],
+  }
 }
 
 function gameLoop() {
   // Main game loop
-  Mousetrap.bind('up', () => {
-      drawCircle(100, 75, 10)
-  })
+  const timer = process.hrtime()
 
-  // Clear screen
-  drawRectangle(0, 0, 800, 600, 'black', true)
-  setTimeout(gameLoop, 1000)
+  // Handling user input
+  Mousetrap.bind('up', () => { keystate.ver-- })
+  Mousetrap.bind('down', () => { keystate.ver++ })
+  Mousetrap.bind('left', () => { keystate.hor-- })
+  Mousetrap.bind('right', () => { keystate.hor++ })
+  
+  // Applying signum function to keystates
+  for (let dir in keystate) {
+    if (keystate[dir] != 0) {
+      keystate[dir] = Math.sign(keystate[dir])
+    }
+  }
+
+  // Updating player object
+  objs.player.drawArgs[0] += (SPEED * keystate.hor)
+  objs.player.drawArgs[1] += (SPEED * keystate.ver)
+
+  // TODO: Update game objects
+
+  // Clearing screen
+  drawRectangle(0, 0, WIDTH, HEIGHT, 'black', true)
+
+  // Drawing all objects
+  for (let obj in objs) {
+    objs[obj].draw(...objs[obj].drawArgs)
+  }
+
+  // Looping back
+  const elapsed = process.hrtime(timer)[1] / 1000000
+  setTimeout(gameLoop, 16 - elapsed)
 }
 
 function gameMain() {
@@ -64,4 +98,4 @@ function gameMain() {
 }
 
 // To export variables to main.js
-module.exports = { WIDTH: W, HEIGHT: H, DEBUG: DEBUG }
+module.exports = { WIDTH: WIDTH, HEIGHT: HEIGHT, DEBUG: DEBUG }
